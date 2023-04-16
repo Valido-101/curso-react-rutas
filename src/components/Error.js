b4035d2a1aec95a77aa8887038dfdb18e4138757
Valido-101 <jesuszafra22@gmail.com>
2023-04-16 14:10:40 +0200

    Enlace fuera de menú de navegación

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Error = () => {
   return (
     <div>
         <h1>ERROR 404</h1>
         <p>La página no existe</p>
+        <Link to="/inicio">Vuelve al inicio</Link>
     </div>
   )
 }
